6a41d8c18dc1879ea0905d57779ecde345dc1629
Adham167 <adhamabdelsalam3040@gmail.com>
2025-10-31 03:47:53 +0200

    add AppColors file

---
+++ lib/core/constants/app_colors.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/cupertino.dart';
+
+class AppColors {
+    static const Color Rectangle_1 = Color(0xFF51526C);
+    static const Color Rectangle_2 = Color(0xFF7993C5);
+    static const Color Rectangle_3 = Color(0xFF8C99B2);
+    static const Color Rectangle_4 = Color(0xFF3F80FF);
+    static const Color Rectangle_5 = Color(0xFF5286EC);
+    static const Color Rectangle_6 = Color(0xFF658DD9);
+    static const Color Dark_Red_100 = Color(0xFFFF3F50);
+    static const Color Dark_Red_200 = Color(0xFFFE0017);
+    static const Color Dark_Red_500 = Color(0xFFBF0011);
+    static const Color Dark_Red_700 = Color(0xFF7F000B);
+    static const Color Dark_Red_900 = Color(0xFF400006);
+    static const Color Dark_Blue_100 = Color(0xFF3F80FF);
+    static const Color Dark_Blue_200 = Color(0xFF0056FE);
+    static const Color Dark_Blue_500 = Color(0xFF0041BF);
+    static const Color Dark_Blue_700 = Color(0xFF002B7F);
+    static const Color Dark_Blue_900 = Color(0xFF001640);
+    static const Color Light_Blue_100 = Color(0xFF3F80FF);
+    static const Color Light_Blue_200 = Color(0xFF659AFF);
+    static const Color Light_Blue_500 = Color(0xFF8CB3FF);
+    static const Color Light_Blue_700 = Color(0xFFB2CCFF);
+    static const Color Light_Blue_900 = Color(0xFFD9E6FF);
+    
+    
+}
