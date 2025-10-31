993386f80b08ff0d12d71340a87edc5e7d7f8d99
Adham167 <adhamabdelsalam3040@gmail.com>
2025-10-31 02:38:02 +0200

    put structure of the project

---
+++ lib/app/marketia.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class Mareketia extends StatelessWidget {
+  const Mareketia({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
