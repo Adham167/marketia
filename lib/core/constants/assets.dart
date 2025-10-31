cfc2cd5944e8cfb0830e0b90ecffb10cadd36fea
Adham167 <adhamabdelsalam3040@gmail.com>
2025-10-31 02:54:18 +0200

    creating splash screen file and adding some of assets

---
+++ lib/core/constants/assets.dart
@@ -0,0 +1,21 @@
+// ignore_for_file: prefer_single_quotes
+class Assets {
+  Assets._();
+  
+  /// Assets for imagesIllustrationOnboarding1
+  /// assets/images/Illustration_Onboarding_1.png
+  static const String imagesIllustrationOnboarding1 = "assets/images/Illustration_Onboarding_1.png";
+
+  /// Assets for imagesIllustrationOnboarding2
+  /// assets/images/Illustration_Onboarding_2.png
+  static const String imagesIllustrationOnboarding2 = "assets/images/Illustration_Onboarding_2.png";
+
+  /// Assets for imagesIllustrationOnboarding3
+  /// assets/images/Illustration_Onboarding_3.png
+  static const String imagesIllustrationOnboarding3 = "assets/images/Illustration_Onboarding_3.png";
+
+  /// Assets for imagesLogoSplashScreen
+  /// assets/images/Logo_Splash_Screen.png
+  static const String imagesLogoSplashScreen = "assets/images/Logo_Splash_Screen.png";
+}
+
