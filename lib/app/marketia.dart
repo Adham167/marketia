51a05818173a24c96333c2ba488799db3f5b58d7
Adham167 <adhamabdelsalam3040@gmail.com>
2025-10-31 02:44:18 +0200

    add class AppRouter for routing of project

--- lib/app/marketia.dart
+++ lib/app/marketia.dart
@@ -1,10 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:marketia/app/router/app_router.dart';
 
 class Mareketia extends StatelessWidget {
   const Mareketia({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return MaterialApp.router(
+      debugShowCheckedModeBanner: false,
+      routerConfig: AppRouter.router,
+    );
   }
 }
